52bbb6bbe0d006f555155330911542bfc91d0825
AugustineOchieng <gusochieng@gmail.com>
2019-03-29 19:13:49 +0300

    created empty repo

---
+++ src/app/repository/repositories.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+// import { Repositories } from "../repository"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RepositoriesService {
+
+  constructor() { }
+}
