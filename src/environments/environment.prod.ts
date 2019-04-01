f41f270a4fbc2a5ac74e04cd8ac9ac5d0d932876
AugustineOchieng <gusochieng@gmail.com>
2019-04-01 16:33:52 +0300

    add header

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://api.github.com/users/",
+  access_token: "?access_token=d5312bfa34a4a4a806bab9de7729aeb4840fabb0"
 };
