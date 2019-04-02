c83466918af22eaad0c3a86cc167e7be9df3d675
AugustineOchieng <gusochieng@gmail.com>
2019-04-02 10:59:13 +0300

    added font style

--- src/app/repository/repositories.service.ts
+++ src/app/repository/repositories.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { User } from '../user';
+import { Repo } from '../repo';
 import { Repository } from '../repository';
 import { environment } from 'src/environments/environment';
 
@@ -13,51 +14,43 @@ import { environment } from 'src/environments/environment';
 })
 export class RepositoriesService {
   user: User;
-  repo: Repository;
+  repo: Repo[];
   public username: string;
-
+  
+  constructor(public http: HttpClient) {
+    this.user = new User('', '', '', '', '', '', 0, 0, 0, '', '', '');
+    this.repo = [];
+    this.username = "AugustineOchieng";
+  }
 
  
+  findInfo() {
 
-  constructor(public http: HttpClient) {
-    this.user = new User("", "", "", "", "", "", 0, 0, 0, "", "","");
-    this.repo = new Repository();
-    this.username = "AugustineOchieng"
-  }
-  getInfo() {
-    interface UserInfo {
-      login: string;
-      html_url: any;
-      avatar_url: string;
-      public_repos: any;
-      repo_url: any;
-      bio: any;
-      created_at: number;
-      following: number;
-      followers: number;
-      location: string;
-      hireable: string;
+    interface APInfo {
+      name: string;
+      html_url: string;
+      description: string;
+
     }
-  
+
     let promise = new Promise((resolve, reject) => {
-      this.http.get<UserInfo>(environment.apiUrl + this.username + environment.access_token).toPromise().then(response => {
-        this.user.login = response.login
-        this.user.avatar_url = response.avatar_url
-        this.user.public_repos = response.public_repos
-        this.user.followers = response.followers
-        this.user.following = response.following
-        this.user.html_url = response.html_url
-        this.user.created_at = response.created_at
-        this.user.bio = response.bio
-        console.log(this.user);
+      this.http.get<APInfo[]>(environment.apiUrl + this.username + "/repos" + environment.access_token).toPromise().then(response => {
+
+        response.forEach(repo => {
+          this.repo.push(new Repo(repo.name, repo.html_url, repo.description))
+        });
+
+        
         resolve()
       }, error => {
-        this.user.login = "Error Fetching Data"
-        this.user.avatar_url = "Error Fetching Data"
-        this.user.public_repos = "Error"
+        
         reject(error)
       })
     })
     return promise
+  }
+
+  update(username: string) {
+    this.username = username;
   }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -4,23 +4,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProfilesService } from '../profile/profiles.service';
 import { RepositoriesService } from '../repository/repositories.service';
 import { User } from '../user';
+import { Repo } from '../repo';
 
 @Component({
   selector: 'app-search',
 
   templateUrl: './search.component.html',
-  providers: [ProfilesService],
+  providers: [ProfilesService, RepositoriesService],
 
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
   user: User;
+  repo: Repo[];
 
   // repos: any[];
 
   public username: string;
 
-  constructor(public profileService: ProfilesService) { }
+  constructor(public profileService: ProfilesService, public repositoryService: RepositoriesService) { }
 
   // findProfile() {
   //   this.profileService.updateProfile(this.username);
@@ -30,17 +32,23 @@ export class SearchComponent implements OnInit {
   //   });
 
   // this.profileService.getProfileRepos().subscribe(repos => {
-  //   this.repos = repos;
+  //   this.repos = repos;#91d370#91d370
   // });
   
   searcher() {
     this.profileService.update(this.username);
     this.profileService.findInfo();
+    this.repositoryService.findInfo();
   }
 
   ngOnInit() {
     this.profileService.findInfo();
     this.user = this.profileService.user;
+    this.repositoryService.findInfo();
+    this.repo = this.repositoryService.repo;
 
   }
 }
+
+
+//
